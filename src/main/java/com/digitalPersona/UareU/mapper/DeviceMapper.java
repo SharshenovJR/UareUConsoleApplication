2527334f1b4f7a1737f42316f68cb2ef44e7e790
SharshenovJR <sharshenov.jr@gmail.com>
2024-05-20 12:44:11 +0600

    updated DeviceDto and method getDevices

--- src/main/java/com/digitalPersona/UareU/mapper/DeviceMapper.java
+++ src/main/java/com/digitalPersona/UareU/mapper/DeviceMapper.java
@@ -11,9 +11,10 @@ public class DeviceMapper {
     private final DeviceDescriptionMapper deviceDescriptionMapper;
     private final DeviceCapabilityMapper deviceCapabilityMapper;
 
-    public DeviceDto toDto(Reader reader) {
+    public DeviceDto toDto(Reader reader, long seq) {
         if (reader == null) return null;
         return DeviceDto.builder()
+                .deviceSeq(seq)
                 .description(deviceDescriptionMapper.toDto(reader.GetDescription()))
                 .capability(deviceCapabilityMapper.toDto(reader.GetCapabilities()))
                 .build();
